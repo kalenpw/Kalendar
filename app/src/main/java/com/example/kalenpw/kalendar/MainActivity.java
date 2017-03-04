c43d690152e4a2c7526e7146ff9bad09c68f4ad7
kalenpw <kalenpwilliams@gmail.com>
2017-03-03 22:31:27 -0700

    Cleaned up imports added license

--- app/src/main/java/com/example/kalenpw/kalendar/MainActivity.java
+++ app/src/main/java/com/example/kalenpw/kalendar/MainActivity.java
@@ -1,29 +1,32 @@
+/*
+    kalenpw
+    kalenpwilliams@gmail.com
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    For a copy of the GNU General Public License see
+    http://www.gnu.org/licenses/
+ */
+
 package com.example.kalenpw.kalendar;
 
 import android.content.Context;
-import android.hardware.input.InputManager;
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.InputType;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.EditText;
-
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     CalendarView _Calendar = null;
